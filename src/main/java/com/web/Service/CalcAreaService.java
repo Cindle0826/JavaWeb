7658d8d4c76f16beeef7bb023ef78af1759169b7
No Name <No Name@Chen-Yi-Hao>
2021-06-07 21:27:45 +0800

    JavaWeb

--- src/main/java/com/web/Service/CalcAreaService.java
+++ src/main/java/com/web/Service/CalcAreaService.java
@@ -1,8 +1,25 @@
 package com.web.Service;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class CalcAreaService {
 
+    private static Map<Integer,String> types;
+    
+    static {
+        types = new LinkedHashMap<>();
+        types.put(1, "圓面積");
+        types.put(2, "球體積");
+        
+    }
+    
+    public String getNameByType(int type){
+        return types.get(type);
+    }
+    
     public double getAreaResult(int type, int r) {
+        
         double area = 0;
 
         switch (type) {
