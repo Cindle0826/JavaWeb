49db6511cffa6612dc5c6199fdc31bbaf1f8b634
No Name <No Name@Chen-Yi-Hao>
2021-06-19 22:51:39 +0800

    JavaWeb

--- src/main/java/com/web/controller/UploadServlet.java
+++ src/main/java/com/web/controller/UploadServlet.java
@@ -45,10 +45,21 @@ public class UploadServlet extends HttpServlet {
                 .forEach(part -> {
                     try {
                         String cname =  IOUtils.toString(part.getInputStream(), StandardCharsets.UTF_8.name());//API apache Apache Commons IO » 2.6
-                        resp.getWriter().print(part.getName().toString() + "<br />");
+                        resp.getWriter().print(part.getName().toString() + ":");
                         resp.getWriter().print(cname + "<br  / >");
                     } catch (Exception e) {
-
+                    }
+                });
+        
+        req.getParts()
+                .stream()
+                .filter(part -> part.getName().equals("upload_file"))
+                .forEach(part -> {
+                    try {
+                        String data =  IOUtils.toString(part.getInputStream(), StandardCharsets.UTF_8.name());//API apache Apache Commons IO » 2.6
+                        resp.getWriter().print(part.getName().toString() + ":");
+                        resp.getWriter().print(data + "<br  / >");
+                    } catch (Exception e) {
                     }
                 });
     }
